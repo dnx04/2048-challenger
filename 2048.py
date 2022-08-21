72c00e8bba186af04ab4a1668ab4138a183466c3
Duong Nguyen <dnx04.gg@gmail.com>
2022-08-21 20:39:05 +0700

    Employed stronger algorithm

---
+++ 2048.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+''' Help the user achieve a high score in a real game of 2048 by using a move searcher. '''
+
+from __future__ import print_function
+import ctypes
+import math
+import time
+import os
+import game
+
+# Enable multithreading?
+MULTITHREAD = True
+
+for suffix in ['so', 'dll', 'dylib']:
+    dllfn = 'bin/2048.' + suffix
+    if not os.path.isfile(dllfn):
+        continue
+    ailib = ctypes.CDLL(dllfn)
+    break
+else:
+    print("Couldn't find 2048 library bin/2048.{so,dll,dylib}! Make sure to build it first.")
+    exit()
+
+ailib.init_tables()
+
+ailib.find_best_move.argtypes = [ctypes.c_uint64]
+ailib.score_toplevel_move.argtypes = [ctypes.c_uint64, ctypes.c_int]
+ailib.score_toplevel_move.restype = ctypes.c_float
+
+def to_c_board(m):
+    board = 0
+    i = 0
+    for row in m:
+        for c in row:
+            board |= int(c) << (4*i)
+            i += 1
+    return board
+
+def _to_val(c):
+    if c == 0: return 0
+    return 2**c
+
+def to_val(m):
+    return [[_to_val(c) for c in row] for row in m]
+
+def _to_score(c):
+    if c <= 1:
+        return 0
+    return (c-1) * (2**c)
+
+def to_score(m):
+    return [[_to_score(c) for c in row] for row in m]
+
+def print_board(m):
+    for row in m:
+        for c in row:
+            print('%8d' % c, end=' ')
+        print()
+
+if MULTITHREAD:
+    from multiprocessing.pool import ThreadPool
+    pool = ThreadPool(4)
+    def score_toplevel_move(args):
+        return ailib.score_toplevel_move(*args)
+
+    def find_best_move(m):
+        board = to_c_board(m)
+        scores = pool.map(score_toplevel_move, [(board, move) for move in range(4)])
+        bestmove, bestscore = max(enumerate(scores), key=lambda x:x[1])
+        if bestscore == 0:
+            return -1
+        return bestmove
+else:
+    def find_best_move(m):
+        board = to_c_board(m)
+        return ailib.find_best_move(board)
+
+def movename(move):
+    return ['up', 'down', 'left', 'right'][move]
+
+def play_game():
+    account = "ayakain5rolls"
+    quiz = 1
+    client = game.Client(account, quiz)
+    while client.playing:
+        data = client.get_state()
+        if isinstance(data, game.Board):
+            for i in range(4):
+                for j in range(4):
+                    if data.board[i][j] != 0:
+                        data.board[i][j] = int(math.log2(data.board[i][j]))
+            client.make_move(movename(find_best_move(data.board)))
+        elif isinstance(data, game.Game):
+            print("Game over!")
+        elif isinstance(data, game.Result):
+            print("Session over!")
+            break
+
+if __name__ == '__main__':
+    play_game()
